c7316d1a037aa095bc8e25084ed06e9df3e174b7
repositriojosegit <100087767+repositriojosegit@users.noreply.github.com>
2023-02-08 19:17:12 -0500

    Add files via upload

---
+++ logica.js
@@ -0,0 +1,6 @@
+function sumar() {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var resultado = num1 + num2;
+    document.getElementById("resultado").innerHTML = "El resultado es: " + resultado;
+}
